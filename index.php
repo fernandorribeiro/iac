a15b514c4bed46afa68c4b804a04f789245a35ea
fernandorribeiro <50499993+fernandorribeiro@users.noreply.github.com>
2022-08-02 15:34:01 -0300

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ $conn = mysqli_connect($servername, $username, $password,$db);
 if (!$conn) {
    die("Connection failed: " . mysqli_connect_error());
 }
-echo "  Conectado com sucesso !!!";
+echo "   Conectado com sucesso !!!";
 ?>
 
 </body>
