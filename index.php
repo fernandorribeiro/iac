27e9bf7f6f4024d5865b0511a351bae839703a50
fernandorribeiro <50499993+fernandorribeiro@users.noreply.github.com>
2022-08-02 14:23:24 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "localhost";
+$servername = "bra-mysqlserver.mysql.database.azure.com";
 $username = "adminuser@bra-mysqlserver";
 $password = "Password123!";
 $db = "bra-mysqlserverdb";
