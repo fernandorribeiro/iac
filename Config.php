1c318813cafe2ea6ee0ab00c17baed8b8b4d340b
fernandorribeiro <50499993+fernandorribeiro@users.noreply.github.com>
2022-08-03 11:02:40 -0300

    Delete Config.php

--- Config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'bra-mysqlserver.mysql.database.azure.com');
-   define('DB_USERNAME', 'adminuser@bra-mysqlserver');
-   define('DB_PASSWORD', 'Password123!');
-   define('DB_DATABASE', 'bra-mysqlserverdb');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
