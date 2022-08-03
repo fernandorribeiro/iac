bbefaf32a591b96ecc855bccdb6aa730afd41a0f
fernandorribeiro <50499993+fernandorribeiro@users.noreply.github.com>
2022-08-03 10:51:57 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -4,7 +4,7 @@ $username = "adminuser@bra-mysqlserver";
 $password = "Password123!";
 $db = "bra-mysqlserverdb";
 // Create connection
-$conn = mysqli_reconnect($servername, $username, $password,$db);
+$conn = mysqli_connect($servername, $username, $password,$db);
 // Check connection
 if (!$conn) {
    die("Connection failed: " . mysqli_connect_error());
